404070f3b74b45e58fa6134fa26bac9c4e56fbe9
jmcnulty7070 <48773406+jmcnulty7070@users.noreply.github.com>
2025-08-07 11:42:24 -0400

    Update lidar_to_mavlink_bridge.py

--- lidar_to_mavlink_bridge.py
+++ lidar_to_mavlink_bridge.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
+
 import rospy
 from sensor_msgs.msg import LaserScan
 from mavros_msgs.msg import DistanceSensor
 
 def callback(data):
+    # Prepare a DistanceSensor message
     ds = DistanceSensor()
     ds.header.stamp = rospy.Time.now()
-    ds.min_distance = 0.1
-    ds.max_distance = 12.0
+    ds.header.frame_id = "lidar_front"
+    ds.min_distance = 0.1        # Minimum distance LiDAR can measure
+    ds.max_distance = 12.0       # Maximum distance LiDAR can measure
     ds.current_distance = min(data.ranges)
-    ds.type = 1
+    ds.type = DistanceSensor.LASER
     ds.id = 0
-    ds.orientation = 0
+    ds.orientation = DistanceSensor.ORIENTATION_FORWARD
+
     pub.publish(ds)
 
-rospy.init_node('lidar_bridge')
-pub = rospy.Publisher('/mavros/distance_sensor/custom', DistanceSensor, queue_size=10)
-rospy.Subscriber('/scan', LaserScan, callback)
-rospy.spin()
+if __name__ == '__main__':
+    rospy.init_node('lidar_to_mavlink_bridge')
+    pub = rospy.Publisher('/mavros/distance_sensor/custom', DistanceSensor, queue_size=10)
+    rospy.Subscriber('/scan', LaserScan, callback)
+    rospy.spin()
